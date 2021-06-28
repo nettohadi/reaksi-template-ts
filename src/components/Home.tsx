a7f960b7e34dac177fedd649362573585661abfa
Hadi Syahbal <hadi.syahbal@gmail.com>
2021-06-28 10:06:51 +0800

    add babel-plugin-auto-import to auto import Reaksi in component when needed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,20 @@
 import Reaksi from "reaksi";
 
 export default function Home(){
+    const [count, setCount] = Reaksi.useState(0);
+
+    Reaksi.useEffect(() => {
+        if(count > 5 )console.log('count is greater than 5');
+    });
+
     return (
         <div>
             <h1>Home</h1>
+            <h1>Count : {count}</h1>
+            <div>
+                <button onclick={() => setCount(count => count + 1)}>+</button>
+                <button onclick={() => setCount(count => count - 1)}>-</button>
+            </div>
         </div>
     );
 }
